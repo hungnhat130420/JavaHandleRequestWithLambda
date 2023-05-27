4692a41f77cd61efe3b65b03b26c526786b1cd7f
Hung Nguyen <hungnguyen.130420@gmail.com>
2023-05-27 15:26:07 +0700

    update

--- src/main/java/com/awsdevelop/GetUserHandler.java
+++ src/main/java/com/awsdevelop/GetUserHandler.java
@@ -1,2 +1,13 @@
-package com.awsdevelop;public class GetUserHandler {
+package com.awsdevelop;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class GetUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        return null;
+    }
 }
--- src/main/java/com/awsdevelop/Main.java
+++ src/main/java/com/awsdevelop/Main.java
@@ -1,5 +1,8 @@
-package com.awsdevelop;public class Main {
+package com.awsdevelop;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
     }
 }
